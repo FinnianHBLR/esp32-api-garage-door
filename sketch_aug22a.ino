71f988d85cd5f190b040a43a0eb290e1cd9e210f
FinnianLR <finnianhlr@gmail.com>
2026-04-13 17:50:59 +1200

    feat: rebooting

--- sketch_aug22a.ino
+++ sketch_aug22a.ino
@@ -1,36 +1,39 @@
 #include <WiFi.h>
 #include <aWOT.h>
-
+// Wemos lolin32
 WiFiServer server(80);
 Application app;
-IPAddress ip();    
+
 IPAddress local_IP(192, 168, 1, 144);
 IPAddress gateway(192, 168, 1, 1);
 IPAddress subnet(255, 255, 255, 0);
+
+// Track uptime for reboot
+unsigned long startMillis;
 
 void removeQuotes(char* str) {
   int i = 0, j = 0;
   while (str[i] != '\0') {
     if (str[i] != '"') {
-      str[j++] = str[i];  // Copy characters that are not quotes
+      str[j++] = str[i];
     }
     i++;
   }
-  str[j] = '\0';  // Null-terminate the cleaned string
+  str[j] = '\0';
 }
 
 void toggleDoor(Request &req, Response &res) {
-  char name[15];
-  char value[15];
+  char name[32];
+  char value[32];
 
   while (req.left()) {
-    if (!req.form(name, 15, value, 15)) {
+    if (!req.form(name, 32, value, 32)) {
       return res.sendStatus(400);
     }
 
     removeQuotes(name);
-
-    if(strcmp(name, "") == 0){
+    // ADD: Password check
+    if (strcmp(name, "") == 0) {
       digitalWrite(13, HIGH);
       delay(1000);
       digitalWrite(13, LOW);
@@ -46,12 +49,13 @@ void toggleDoor(Request &req, Response &res) {
 void setup() {
   pinMode(13, OUTPUT);
 
-  if (!WiFi.config(local_IP, gateway, subnet)) {
-  }
-  
+  startMillis = millis();
+
+  WiFi.config(local_IP, gateway, subnet);
   WiFi.setSleep(false);
   WiFi.setAutoReconnect(true);
-  
+
+  // ADD: SSID + PASS
   WiFi.begin("", "");
 
   while (WiFi.status() != WL_CONNECTED) {
@@ -63,11 +67,38 @@ void setup() {
   server.begin();
 }
 
-void loop() {  
+void loop() {
+  // ✅ 1. Periodic reboot (every 3 days)
+  if (millis() - startMillis > 1000UL * 60 * 60 * 24 * 3) {
+    ESP.restart();
+  }
+
+  // ✅ 2. WiFi self-healing
+  if (WiFi.status() != WL_CONNECTED) {
+    WiFi.disconnect();
+  // ADD: SSID + PASS
+    WiFi.begin("", "");
+    return; // Skip loop until reconnected
+  }
+
+  // ✅ 3. Safer client handling
   WiFiClient client = server.available();
 
-  if (client.connected()) {
-    app.process(&client);
+  if (client) {
+    unsigned long timeout = millis();
+
+    while (client.connected() && !client.available()) {
+      if (millis() - timeout > 2000) {
+        client.stop();
+        return;
+      }
+      delay(1);
+    }
+
+    if (client.available()) {
+      app.process(&client);
+    }
+
     client.stop();
   }
 }
